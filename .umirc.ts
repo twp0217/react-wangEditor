0b0a3771e6bfc8b654e72c678f16b6407fc27a87
twp0217 <tu.wenpan@qq.com>
2021-09-21 12:42:51 +0800

    ci: 更新demo配置

--- .umirc.ts
+++ .umirc.ts
@@ -6,7 +6,7 @@ export default defineConfig({
     'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   outputPath: 'docs-dist',
-  base: '/react-wangeditor',
-  publicPath: '/react-wangeditor/',
+  base: '/react-wangEditor',
+  publicPath: '/react-wangEditor/',
   // more config: https://d.umijs.org/config
 });
